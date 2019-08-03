58ac3af8332483a3a965b0fc3c8215a322c9e180
Adrimi <adriano100417@gmail.com>
2019-08-03 10:07:53 +0200

    changed rectangles to balls

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -11,6 +11,47 @@ import SpriteKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
-      
+        
+        // create background
+        let background = SKSpriteNode(imageNamed: "background")
+        
+        // set its position to center of the iPad view
+        background.position = CGPoint(x: 512, y: 384)
+        
+        // ignore alpha value
+        background.blendMode = .replace
+        
+        // draw behind, cause it's background
+        background.zPosition = -1
+        
+        // register background
+        addChild(background)
+        
+        // register physics, the area of active physics is whole scene!
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // get first tap
+        guard let touch = touches.first else { return }
+        
+        // where it was tapped (get the location)
+        let location = touch.location(in: self)
+        
+        // create view based on image(and alpha)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        
+        // add physics
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        
+        // add bounciness
+        ball.physicsBody?.restitution = 0.4
+        
+        // relocate
+        ball.position = location
+        
+        // register to main view
+        addChild(ball)
     }
 }
