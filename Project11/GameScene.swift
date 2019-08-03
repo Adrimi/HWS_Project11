7193eed6da0476043152c3660678a661fb1c0594
Adrimi <adriano100417@gmail.com>
2019-08-03 12:43:15 +0200

    collision testing, but still problem with fatal error (propably when two notifies comes at once)

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
     override func didMove(to view: SKView) {
         
@@ -29,14 +29,17 @@ class GameScene: SKScene {
         
         // register physics, the area of active physics is whole scene!
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
+        for x in 0..<5 {
+            makeSlot(at: CGPoint(x: x * 256 + 128, y: 0), isGood: x % 2 == 0 ? true : false)
+        }
         
         for x in 0...5 {
             makeBouncer(at: CGPoint(x: x * 256, y: 0))
         }
-        
-        for x in 0...5 {
-            makeSlot(at: CGPoint(x: x * 256 + 128, y: 0), isGood: x % 2 == 0 ? true : false)
-        }
+                
+        // conform to contact delegate
+        physicsWorld.contactDelegate = self
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -56,8 +59,16 @@ class GameScene: SKScene {
         // add bounciness
         ball.physicsBody?.restitution = 0.4
         
+        // register view to contact delegate.
+        // CollisionBitMask tells what views are desired to contact (default - everything on a scene).
+        // ContactTestBitMask tells of what contact delegate will notify
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+        
         // relocate
         ball.position = location
+        
+        // name to ball
+        ball.name = "ball"
         
         // register to main view
         addChild(ball)
@@ -79,13 +90,19 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         
         slotBase.position = position
         slotGlow.position = position
+        
+        // physics to slot
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
         
         addChild(slotGlow)
         addChild(slotBase)
@@ -94,5 +111,29 @@ class GameScene: SKScene {
         let spin = SKAction.rotate(byAngle: .pi, duration: 10)
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
+    }
+    
+    // when contact between two object is happening, we don't know about the order of contact (is 1. body to 2. body / or 2. to 1. / both!)
+    // but it is important to catch Ball collision to node there!
+    func collision(between ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+    
+    func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "ball" {
+            collision(between: contact.bodyA.node!, object: contact.bodyB.node!)
+        } else if contact.bodyB.node?.name == "ball" {
+            collision(between: contact.bodyB.node!, object: contact.bodyA.node!)
+        }
+        
+        // there is no need to support ball-to-ball contact
     }
 }
